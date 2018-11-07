c1a1f2e71651fe880a4a09fe6a870a6bb7a6e05a
caiomodena <caio.modena@gmail.com>
2018-11-07 20:22:38 -0200

    iniciando projeto com stream

---
+++ FileStreamAndStreamReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileStreamAndStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
