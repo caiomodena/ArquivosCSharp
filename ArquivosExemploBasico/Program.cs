1d9b1be2065ed9a5e10953fc246b007ff2706d0c
caiomodena <caio.modena@gmail.com>
2018-11-07 19:56:59 -0200

    utilizando FileInfo e File

---
+++ ArquivosExemploBasico/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ArquivosExemploBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath=@"C:\Users\caio\Desktop\Teste.txt";
+            string targetPath=@"C:\Users\caio\Desktop\Teste2.txt";
+            string[] lines=File.ReadAllLines(sourcePath);
+            foreach(string line in lines){
+                Console.WriteLine(line);
+            }
+
+            try{
+                FileInfo fileInfo=new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+            }
+            catch(IOException e){
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
