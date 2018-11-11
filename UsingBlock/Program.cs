6c88d7b9a6f9bb582dfc0c209cda6f77f669fcbf
caiomodena <caio.modena@gmail.com>
2018-11-11 09:42:32 -0200

    utilizando using para nao precisar encerrar stream manualmente

---
+++ UsingBlock/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace UsingBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path=@"C:\Users\caio\Desktop\Teste.txt";
+            
+            try{
+                using(FileStream fileStream=new FileStream(path,FileMode.Open)){
+                    using(StreamReader streamReader=new StreamReader(fileStream)){
+                        while(!streamReader.EndOfStream){
+                            Console.WriteLine(streamReader.ReadLine());
+                        }
+                    }
+
+                }
+            }
+            catch(IOException ex){
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
