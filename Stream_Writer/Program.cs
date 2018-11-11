e9a1d7ef85b990950857fc386caba6e4fd0d9083
caiomodena <caio.modena@gmail.com>
2018-11-11 10:01:02 -0200

    utilizando streamWriter para escrever dentro de arquivos

---
+++ Stream_Writer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+namespace Stream_Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath=@"C:\Users\caio\Desktop\Teste.txt";
+            string targetPath=@"C:\Users\caio\Desktop\Teste1.txt";
+            try{
+                string[] lines=File.ReadAllLines(sourcePath);
+                using(StreamWriter streamWriter=File.AppendText(targetPath)){
+                    foreach(string line in lines){
+                        streamWriter.WriteLine(line.ToUpper());
+                    }
+                }
+
+            }
+            catch(IOException e){
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
