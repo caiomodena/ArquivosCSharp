8133e47f628cad72d7eab95bc25513903c589de6
caiomodena <caio.modena@gmail.com>
2018-11-12 20:44:34 -0200

    trabalhando com caminhos de diretorios

---
+++ Path/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace PathEstudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path=@"C:\Users\caio\Desktop\teste\teste1.txt";
+            Console.WriteLine(Path.DirectorySeparatorChar);
+            Console.WriteLine(Path.PathSeparator);
+            Console.WriteLine(Path.GetDirectoryName(path));
+            Console.WriteLine(Path.GetFileName(path));
+            Console.WriteLine(Path.GetExtension(path));
+
+
+        }
+    }
+}
